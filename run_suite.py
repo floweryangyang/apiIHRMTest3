10ee28599346842ef17966ed80ccf694f88007fc
Administrator <qwe@163.com>
2020-01-08 13:28:39 +0800

    人力资源系统

--- run_suite.py
+++ run_suite.py
@@ -12,7 +12,7 @@ suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(Login))
 suite.addTest(unittest.makeSuite(TestIHRMEmp))
 # 3 使用HTMLTestRunner执行测试套件，生成测试报告
-report_path = app.BASE_DIR + "/report/ihrm{}.html".format(time.strftime('%Y%m%d %H%M%S'))
+report_path = app.BASE_DIR + "/report/ihrm.html"
 with open(report_path, mode='wb') as f:
     # 初始化HTMLTestRunner
     runner = HTMLTestRunner(f, verbosity=1, title="IHRM人力资源接口测试", description="V1.0.0")
